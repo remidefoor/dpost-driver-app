bf8e91cad9fd19c4b0e628307d1b214d6baa0540
Rémi Defoor <remi.defoor@student.howest.be>
2022-11-08 07:59:59 +0100

    feat(packages): style packages list

--- src/app/components/packages/packages.service.ts
+++ src/app/components/packages/packages.service.ts
@@ -33,6 +33,72 @@ export class PackagesService {
         dest: "Dome 11 H97",
         fee: 3.0
       },
+      {
+        size: "Small",
+        src: "Dome 9 H1B",
+        dest: "Dome 9 H11",
+        fee: 1.50
+      },
+      {
+        size: "Small",
+        src: "Dome 9 H19",
+        dest: "Dome 9 H113",
+        fee: 1.75
+      },
+      {
+        size: "Letter",
+        src: "Dome 11 H19",
+        dest: "Dome 9 H113",
+        fee: 2.25
+      },
+      {
+        size: "Large",
+        src: "Dome 9 H3",
+        dest: "Dome 11 H97",
+        fee: 3.0
+      },
+      {
+        size: "Small",
+        src: "Dome 9 H1B",
+        dest: "Dome 9 H11",
+        fee: 1.50
+      },
+      {
+        size: "Small",
+        src: "Dome 9 H19",
+        dest: "Dome 9 H113",
+        fee: 1.75
+      },
+      {
+        size: "Letter",
+        src: "Dome 11 H19",
+        dest: "Dome 9 H113",
+        fee: 2.25
+      },
+      {
+        size: "Large",
+        src: "Dome 9 H3",
+        dest: "Dome 11 H97",
+        fee: 3.0
+      },
+      {
+        size: "Small",
+        src: "Dome 9 H1B",
+        dest: "Dome 9 H11",
+        fee: 1.50
+      },
+      {
+        size: "Small",
+        src: "Dome 9 H19",
+        dest: "Dome 9 H113",
+        fee: 1.75
+      },
+      {
+        size: "Letter",
+        src: "Dome 11 H19",
+        dest: "Dome 9 H113",
+        fee: 2.25
+      }
     ];
   }
 
