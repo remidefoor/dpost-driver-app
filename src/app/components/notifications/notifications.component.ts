1c3f51c757c6a4b53c5206d76fff4591a1d6aad2
Rémi Defoor <remi.defoor@student.howest.be>
2022-11-28 09:14:06 +0100

    feat(notifications): create socket connection

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotificationsService } from '../../services/notifications.service';
 
 @Component({
@@ -6,12 +6,25 @@ import { NotificationsService } from '../../services/notifications.service';
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit{
 
   notifications: string[];
 
+  notificationList: any[];
+
   constructor(private readonly notificationService: NotificationsService) {
     this.notifications = notificationService.notifications;
+
+    this.notificationList = [];
+  }
+
+  ngOnInit() {
+    console.log('register');
+    this.notificationService.register();
+    this.notificationService.getNotification().subscribe((notification: any) => {
+      console.log(notification);
+      this.notificationList.push(notification);
+    });
   }
 
   deleteNotifications(): void {
--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
+  socket: Socket;
+
   notifications: string[];
 
+  notification: BehaviorSubject<any>;
+
   constructor() {
+    this.socket = io('https://state-notifier.dpost.online/');
+
     this.notifications = [];
     this.listenForNotifications();
+
+    this.notification = new BehaviorSubject('');
   }
 
   private listenForNotifications(): void {
     this.notifications.push('Pickup has been approved!');
     this.notifications.push('You\'ve been paid $ 6.25!');
   }
+
+  register(): void {
+    this.socket.emit('hello', { clientId: 'ako-18' });
+  }
+
+  getNotification = (): Observable<any> => {
+    this.socket.on('stateChange', (notification: any) => {
+      this.notification.next(notification);
+    });
+    return this.notification.asObservable();
+  };
 
 }
