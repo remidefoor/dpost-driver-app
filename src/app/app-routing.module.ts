dec87abb86a3e28e1136b1c5d49242cecf761f3f
Rémi Defoor <remi.defoor@student.howest.be>
2022-11-28 09:27:49 +0100

    feat(delivery): create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PackagesComponent} from './components/packages/packages.component';
 import { PackageComponent } from './components/package/package.component';
+import { DeliveryComponent } from "./components/delivery/delivery.component";
 import { NotificationsComponent } from './components/notifications/notifications.component';
 
 const routes: Routes = [
   { path: 'packages', component: PackagesComponent },
   { path: 'packages/:packageId', component: PackageComponent },
+  { path: 'delivery', component: DeliveryComponent },
   { path: 'notifications', component: NotificationsComponent },
   { path: '', redirectTo: '/packages', pathMatch: 'full' }
 ];
