f64646e565c101cb128306e117713314314b8c69
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-08 19:02:05 +0100

    refactor(delivery): apply constant notation

--- src/app/components/delivery/delivery.component.ts
+++ src/app/components/delivery/delivery.component.ts
@@ -11,18 +11,19 @@ import {ApiService} from '../../services/api.service';
 })
 export class DeliveryComponent {
 
+  QR_CODE_WIDTH: number;
+  QR_CODE_ERROR_CORRECTION_LEVEL: QRCodeErrorCorrectionLevel;
+
   delivery: Delivery | undefined;
-  qrCodeWidth: number;
-  qrCodeErrorCorrectionLevel: QRCodeErrorCorrectionLevel;
 
   constructor(private readonly route: ActivatedRoute, private readonly apiService: ApiService) {
+    this.QR_CODE_WIDTH = 256;
+    this.QR_CODE_ERROR_CORRECTION_LEVEL = 'M';
+
     const routeParams = this.route.snapshot.paramMap;
     const deliveryId = Number(routeParams.get('deliveryId'));
     this.apiService.getDelivery(deliveryId)
       .subscribe(delivery => this.delivery = delivery);
-
-    this.qrCodeWidth = 256;
-    this.qrCodeErrorCorrectionLevel = 'M';
   }
 
 }
