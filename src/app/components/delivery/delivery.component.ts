fdbf5a6fc34fa7432d05ad465edd7e77004612be
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-09 10:33:15 +0100

    refactor(delivery): extend ui update states

--- src/app/components/delivery/delivery.component.ts
+++ src/app/components/delivery/delivery.component.ts
@@ -16,6 +16,7 @@ export class DeliveryComponent implements OnInit {
   QR_CODE_ERROR_CORRECTION_LEVEL: QRCodeErrorCorrectionLevel;
 
   delivery: Delivery | undefined;
+  stateMsg: string;
   instruction: string;
   cta: string;
 
@@ -33,6 +34,7 @@ export class DeliveryComponent implements OnInit {
         this.updateUi();
       });
 
+    this.stateMsg = '';
     this.instruction = '';
     this.cta = '';
   }
@@ -49,12 +51,17 @@ export class DeliveryComponent implements OnInit {
   updateUi(): void {
     switch (this.delivery?.state) {
       case 'transit':
+        this.stateMsg = 'You\'re en route to drop off a package';
         this.instruction = 'Drive to the drop-off location and show the QR code to the receiver';
         this.cta = 'Send drop-off location to car';
         break;
+      case 'delivered':
+          this.stateMsg = 'Great! You\'ll be paid shortly.\nHere\'s a summary of your delivery:';
+        break;
       default:
+        this.stateMsg = 'You\'ve been assigned to the delivery';
         this.instruction = 'Drive to the pick-up location and show the QR code to the sender';
-        this.cta = 'Send pickup location to car';
+        this.cta = 'Send pick-up location to car';
     }
   }
 
